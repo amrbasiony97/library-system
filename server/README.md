f411344ca3010264820ced67b95832dc1152ada3
amrbasiony97 <amrbasiony97@gmail.com>
2023-03-12 23:32:56 +0200

    Initial commit

---
+++ server/README.md
@@ -0,0 +1,33 @@
+# Library Management System
+This is a web application designed to manage library transactions, including adding new members, new books, searching books, members, and facilities to borrow and read books. The system has 4 types of users: Basic Admin, Admin, Employee, and Member.
+
+## Table of Contents
+[Users Rules](#users-roles)  
+[Installation](#installation)  
+[Technologies](#technologies)  
+
+## Users Roles
+- **Basic Administrator:** responsible for managing the entire system, adding or removing any admin from the system, monitoring the entire system data, and updating any system data.
+
+- **Administrator:** responsible for adding or removing employees from the system, adding, removing or updating books, and updating his/her profile.
+
+- **Employee:** responsible for adding or removing members, borrowing books, managing reading books in the library, and updating his/her profile.
+
+- **Member:** can show his/her data on the system like profile, borrowed books, reading books, deadline of borrowing, and updating his/her profile.
+
+## Installation
+- Clone the repository to your local machine
+- Install dependencies using  
+  ```
+  npm install
+  ```
+- Run the application using  
+  ```
+  npm start
+  ```
+
+## Technologies
+- Node.js
+- Express.js
+- MongoDB
+- Angular
---
+++ server/app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+// const adminRouter = require("./Routers/adminRouter");
+// const basicAdminRouter = require("./Routers/basicAdminRouter");
+// const bookRouter = require("./Routers/bookRouter");
+// const employeeRouter = require("./Routers/employeeRouter");
+// const loginRouter = require("./Routers/loginRouter");
+// const memberRouter = require("./Routers/memberRouter");
+// const authenticateMW = require("./Core/Auth/authenticateMW");
+const server = express();
+const port = process.env.PORT || 8080;
+
+// Connect to MongoDB
+mongoose.set("strictQuery", true);
+mongoose
+  .connect("mongodb://127.0.0.1:27017/LIBRARY_MANAGEMENT_SYSTEM")
+  .then(() => {
+    console.log("Database is connected");
+    // Run the server
+    server.listen(port, () => {
+      console.log("Server is Running...");
+    });
+  })
+  .catch((error) => {
+    console.log(`error: ${error}`);
+  });
+
+// Logging Middleware
+server.use(morgan(":url :method"));
+
+// CORS Middleware
+server.use(cors());
+
+// parse-data
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+
+// Routes
+// server.use(loginRouter);
+// server.use(authenticateMW);
+// server.use(adminRouter);
+// server.use(basicAdminRouter);
+// server.use(bookRouter)
+// server.use(memberRouter);
+// server.use(employeeRouter);
+
+// Not Found Middleware
+server.use((request, response) => {
+  response.status(404).json({ message: "Route not found" });
+});
+
+// Error handling Middleware
+server.use((error, request, response, next) => {
+  let status = error.status || 500;
+  response.status(status).json({ message: error + "" });
+});
