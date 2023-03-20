d1cbd43baa041bb351e4de0d5fc61a32e6d96b18
amrbasiony97 <amrbasiony97@gmail.com>
2023-03-20 11:00:56 +0200

    Added user model

---
+++ server/Models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+module.exports = new mongoose.Schema(
+    {
+        _id: Number,
+        firstName: {
+            type: String,
+            required: true
+        },
+        lastName: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        tmpPassword: String,
+        password: String,
+        gender: {
+            type: String,
+            enum: ['male', 'female']
+        },
+        birthDate: Date,
+        image: String
+    },
+    { _id: false }
+);
