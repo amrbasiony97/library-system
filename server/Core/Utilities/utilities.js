62adb9ef3357c56b49fbdfdd57d2895956bfeeb1
amrbasiony97 <amrbasiony97@gmail.com>
2023-03-21 08:04:13 +0200

    Updated utililties.js
    Added checkEmail function to check for email uniqueness across all users in the system
    Added generatedPassword function that takes the length of password as a number and generates a strong password

--- server/Core/Utilities/utilities.js
+++ server/Core/Utilities/utilities.js
@@ -1,3 +1,58 @@
+const mongoose = require('mongoose');
+const UserRoleSchema = mongoose.model('users_roles');
+
 exports.toCapitalCase = str =>
     str.split('/')[1].charAt(0).toUpperCase() +
     str.split('/')[1].substring(1, str.length - 1);
+
+exports.checkEmail = (request, response, next) => {
+    UserRoleSchema.findOne({ email: request.body.email })
+        .then(user => {
+            if (user) {
+                let error = new Error('Email already exists');
+                error.status = 409;
+                next(error);
+            } else next();
+        })
+        .catch(error => {
+            next(error);
+        });
+};
+
+exports.generatePassword = length => {
+    let password = '',
+        charCode;
+
+    // exclude backslash, backtick, single and double quotes
+    let preventChars = [34, 39, 92, 96];
+    for (let char = 0; char < 4; char++) {
+        if (char % 4 == 0) {
+            charCode = Math.floor(Math.random() * 10 + 48);
+        } // include numbers
+        else if (char % 4 == 1) {
+            charCode = Math.floor(Math.random() * 26 + 65);
+        } // include capital letters
+        else if (char % 4 == 2) {
+            charCode = Math.floor(Math.random() * 26 + 97);
+        } // include small letters
+        else {
+            charCode =
+                Math.floor(Math.random() * 15 + 33) ||
+                Math.floor(Math.random() * 7 + 58) ||
+                Math.floor(Math.random() * 6 + 91) ||
+                Math.floor(Math.random() * 4 + 123);
+        } // include special characters
+        if (!preventChars.includes(charCode)) {
+            password += String.fromCharCode(charCode);
+        }
+    }
+    while (password.length < length) {
+        charCode =
+            Math.floor(Math.random() * 59 + 33) ||
+            Math.floor(Math.random() * 34 + 93);
+        if (!preventChars.includes(charCode)) {
+            password += String.fromCharCode(charCode);
+        }
+    }
+    return password;
+};
