9e009f840a9b918f518be5a6381262c780127279
amrbasiony97 <amrbasiony97@gmail.com>
2023-03-21 15:09:54 +0200

    Added employee router

---
+++ server/Routers/employeeRouter.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const controller = require('../Controllers/employeeController');
+const validateMW = require('../Core/Validations/validateMW');
+const validateEmployee = require('../Core/Validations/validateEmployee');
+const multerMW = require('../Core/Multer/multerMW');
+const { checkEmail } = require('../Core/Utilities/utilities');
+
+const router = express.Router();
+
+router
+    .route('/employees')
+    .get(controller.getAllEmployees)
+    .post(
+        multerMW,
+        checkEmail,
+        validateEmployee.validatePostArray,
+        validateMW,
+        controller.addEmployee
+    )
+    .patch(
+        multerMW,
+        checkEmail,
+        validateEmployee.validateAdminPatchArray,
+        validateMW,
+        controller.updateEmployeeByAdmin
+    )
+    // .delete();
+
+router
+    .route('/employees/:id')
+    .get(controller.getEmployeeById)
+    // .patch();
+
+router
+    .route('/employees/activate/:id')
+    // .patch();
+module.exports = router;
